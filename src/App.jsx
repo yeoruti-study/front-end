120eac4ae2d3590c16398ef73f27e7bcfe827ea0
Yongjae Choi <yongjae.choi20@gmail.com>
2023-01-15 21:45:17 +0900

    [여럿이-11] 홈페이지 과목 슬라이더 만들기 (#15)
    * ➕ recoil 추가
    
    * 🔥 App.css 제거
    
    * ✨ 과목 슬라이더 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import GlobalStyles from './style/GlobalStyles';
-import LoginContainer from './container/LoginContainer';
-import { Routes, Route } from 'react-router-dom';
+import GlobalStyles from "./style/GlobalStyles";
+import LoginContainer from "./container/LoginContainer";
+import SubjectContainer from "./containers/SubjectContainer";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (<>
-    <GlobalStyles />
-    <Routes>
-      <Route path='/' element={<LoginContainer />} />
-    </Routes>
+  return (
+    <>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<LoginContainer />} />
+        <Route path="/home" element={<SubjectContainer />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/SlideButton.tsx
@@ -0,0 +1,66 @@
+import React, { MouseEventHandler } from "react";
+import { ReactComponent as Next } from "../assets/icons/chevron-right.svg";
+import { ReactComponent as Prev } from "../assets/icons/chevron-left.svg";
+import styled from "styled-components";
+
+type SlideButtonProps = {
+  direction: string;
+  onClick?: MouseEventHandler;
+  disable?: boolean;
+};
+const SlideButton = (props: SlideButtonProps) => {
+  const { direction, onClick, disable } = props;
+
+  const findButton = (direction: string) => {
+    if (!direction || disable) return <></>;
+    if (direction === "left") {
+      return (
+        <LeftButtonWrapper
+          direction="left"
+          onClick={disable ? () => {} : onClick}
+        >
+          <Prev fill="white" style={{ width: "20px", height: "20px" }} />
+        </LeftButtonWrapper>
+      );
+    } else if (direction === "right") {
+      return (
+        <RightButtonWrapper
+          direction="right"
+          onClick={disable ? () => {} : onClick}
+        >
+          <Next fill="white" style={{ width: "20px", height: "20px" }} />
+        </RightButtonWrapper>
+      );
+    }
+    return <></>;
+  };
+
+  return findButton(direction);
+};
+
+export default SlideButton;
+
+interface ButtonProps {
+  direction: string;
+  onClick?: Function;
+}
+const SlideButtonWrapper = styled.div<ButtonProps>`
+  position: absolute;
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  border: none;
+  background: rgba(0, 0, 0, 0.4);
+  color: white;
+  cursor: pointer;
+`;
+const LeftButtonWrapper = styled(SlideButtonWrapper)`
+  left: 5px;
+`;
+const RightButtonWrapper = styled(SlideButtonWrapper)`
+  right: 5px;
+`;
