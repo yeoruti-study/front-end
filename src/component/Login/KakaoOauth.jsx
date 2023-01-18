708000d8841e410a4bb9270a2e285c677d6275c7
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2023-01-18 23:35:12 +0900

    fix: 카카오 로그인 테스트 포로토타입 개선

--- src/component/Login/KakaoOauth.jsx
+++ src/component/Login/KakaoOauth.jsx
@@ -1,24 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
 const { Kakao } = window;
 
-const JAVASCRIPT_KEY = '3daf56efeb95916bc80ff1e2a9585671';
-const REST_API_KEY = 'b8fc366879c4e2fa9bddef1d943cae7d';
-const ADMIN_KEY = '91fe5e2f4f0c8ea42d27efb169a34a52';
+const JAVASCRIPT_KEY = "3daf56efeb95916bc80ff1e2a9585671";
+const REST_API_KEY = "b8fc366879c4e2fa9bddef1d943cae7d";
+const ADMIN_KEY = "91fe5e2f4f0c8ea42d27efb169a34a52";
 
 export function loginWithKakao() {
   Kakao.init(JAVASCRIPT_KEY);
   console.log(Kakao);
   console.log(Kakao.isInitialized());
   Kakao.Auth.authorize({
-    redirectUri: 'https://localhost:3000/kakao/callback',
+    redirectUri: "http://localhost:3000/kakao/callback",
   });
-};
+}
 
-const url = 'https://kauth.kakao.com/oauth/token';
+const url = "https://kauth.kakao.com/oauth/token";
 const kakaoHeader = {
-  'Authorization': ADMIN_KEY,
-  'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+  Authorization: ADMIN_KEY,
+  "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
 };
 
 // 참고: https://kdinner.tistory.com/92
@@ -27,15 +27,16 @@ export async function requestUserInfo(code) {
   const payload = {
     grant_type: "authorization_code",
     client_id: REST_API_KEY,
-    redirect_uri: "http://localhost:3000/kakao/callback", 
+    redirect_uri: "http://localhost:3000/kakao/callback",
     code,
   };
   const queryString = Object.keys(payload)
-    .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(payload[k]))
-    .join('&');
+    .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(payload[k]))
+    .join("&");
   try {
     const result = await axios.post(url, queryString, { headers: kakaoHeader });
     console.log(result);
+    return result;
   } catch (e) {
     console.log(e);
   }
