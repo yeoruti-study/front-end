8cf4580ffc5b66f883420cce8216f05f98d26cb6
yjc2021 <yongjae.choi20@gmail.com>
2023-03-02 03:21:37 +0900

    fix: StudyHome 더미 데이터 제거

--- src/containers/StudyRoom/StudyHome.tsx
+++ src/containers/StudyRoom/StudyHome.tsx
@@ -2,131 +2,14 @@ import React, { PropsWithChildren } from "react";
 import styled from "styled-components";
 import StudyList, { RoomType } from "../../components/StudyRoom/StudyList";
 import StudyModal from "../../components/StudyRoom/StudyModal/StudyModal";
+import { useRoomUserAllGet } from "../../hooks/react_query_hooks/useRoomUser";
 import { useStudyRoomAllGet } from "../../hooks/react_query_hooks/useStudyRoom";
-const roomListDummy: RoomType[] = [
-  {
-    id: "25e1d2ea-4063-4be5-ba28-ad7c226df2f5",
-    name: "스터디 룸1",
-    studyCategoryDto: {
-      id: "22acdcc3-0282-4031-b2f3-d14aaa1ce0c3",
-      name: "스터디카테고리1",
-      description: "스터디카테고리1의 설명 수정",
-    },
-    maximumNumberOfPeople: 3,
-    studyGoalTime: "",
-    roomPassword: "",
-    masterUserId: "",
-    createdAt: "2023-01-10T21:11:34",
-    updatedAt: "2023-02-11T16:03:31",
-  },
-  {
-    id: "f59cf358-5561-42a4-b2da-8e9ac730b346",
-    name: "스터디 룸3",
-    studyCategoryDto: {
-      id: "1651291b-83ef-40eb-8b24-0b7c5d7250ab",
-      name: "카테고리",
-      description: "카테고리 설명",
-    },
-    maximumNumberOfPeople: 2,
-    studyGoalTime: undefined,
-    roomPassword: undefined,
-    masterUserId: undefined,
-    createdAt: "2023-01-10T21:20:48",
-    updatedAt: undefined,
-  },
-  {
-    id: "eeaa932b-1f91-4d50-ac21-0521cdf4deea",
-    name: "스터디룸123123",
-    studyCategoryDto: {
-      id: "22acdcc3-0282-4031-b2f3-d14aaa1ce0c3",
-      name: "스터디카테고리1",
-      description: "스터디카테고리1의 설명 수정",
-    },
-    maximumNumberOfPeople: 3,
-    studyGoalTime: "PT3H",
-    roomPassword: undefined,
-    masterUserId: "3cfd73fe-e6fa-4a21-9a3a-dd1ba7689431",
-    createdAt: "2023-02-11T11:22:08",
-    updatedAt: undefined,
-  },
-  {
-    id: "8b9b8400-4c72-4163-abb5-6a3b2837b2d4",
-    name: "스터디룸123123",
-    studyCategoryDto: {
-      id: "22acdcc3-0282-4031-b2f3-d14aaa1ce0c3",
-      name: "스터디카테고리1",
-      description: "스터디카테고리1의 설명 수정",
-    },
-    maximumNumberOfPeople: 0,
-    studyGoalTime: "PT3H",
-    roomPassword: undefined,
-    masterUserId: "3cfd73fe-e6fa-4a21-9a3a-dd1ba7689431",
-    createdAt: "2023-02-11T11:42:57",
-    updatedAt: undefined,
-  },
-  {
-    id: "5b40299b-ea36-4da1-8cf4-702ac59ab712",
-    name: "스터디룸123123",
-    studyCategoryDto: {
-      id: "22acdcc3-0282-4031-b2f3-d14aaa1ce0c3",
-      name: "스터디카테고리1",
-      description: "스터디카테고리1의 설명 수정",
-    },
-    maximumNumberOfPeople: 0,
-    studyGoalTime: "PT3H",
-    roomPassword: undefined,
-    masterUserId: "3cfd73fe-e6fa-4a21-9a3a-dd1ba7689431",
-    createdAt: "2023-02-11T11:44:02",
-    updatedAt: undefined,
-  },
-  {
-    id: "e99efa3b-1270-4a4b-8291-364de3fc06c7",
-    name: "스터디룸123123",
-    studyCategoryDto: {
-      id: "22acdcc3-0282-4031-b2f3-d14aaa1ce0c3",
-      name: "스터디카테고리1",
-      description: "스터디카테고리1의 설명 수정",
-    },
-    maximumNumberOfPeople: 30,
-    studyGoalTime: "PT3H",
-    roomPassword: undefined,
-    masterUserId: "3cfd73fe-e6fa-4a21-9a3a-dd1ba7689431",
-    createdAt: "2023-02-11T11:45:41",
-    updatedAt: undefined,
-  },
-  {
-    id: "7f3aed24-5b30-468e-84d2-e1e1e622e29f",
-    name: "스터디룸123123123444",
-    studyCategoryDto: {
-      id: "22acdcc3-0282-4031-b2f3-d14aaa1ce0c3",
-      name: "스터디카테고리1",
-      description: "스터디카테고리1의 설명 수정",
-    },
-    maximumNumberOfPeople: 30,
-    studyGoalTime: "PT3H",
-    roomPassword: undefined,
-    masterUserId: "3cfd73fe-e6fa-4a21-9a3a-dd1ba7689431",
-    createdAt: "2023-02-11T14:04:07",
-    updatedAt: undefined,
-  },
-  {
-    id: "711268c8-373b-406a-a42e-6376593a252a",
-    name: "스터디룸1",
-    studyCategoryDto: {
-      id: "22acdcc3-0282-4031-b2f3-d14aaa1ce0c3",
-      name: "스터디카테고리1",
-      description: "스터디카테고리1의 설명 수정",
-    },
-    maximumNumberOfPeople: 3,
-    studyGoalTime: "PT3H",
-    roomPassword: undefined,
-    masterUserId: "3cfd73fe-e6fa-4a21-9a3a-dd1ba7689431",
-    createdAt: "2023-02-09T20:45:27",
-    updatedAt: undefined,
-  },
-];
+import localConsole from "../../utils/localConsole";
+
 const StudyHome = () => {
   const { status, data } = useStudyRoomAllGet();
+  // const { status, data } = useRoomUserAllGet();
+  if (status === "success") localConsole?.log(data?.data.data);
   return (
     <>
       {/* <StudyModal /> */}
