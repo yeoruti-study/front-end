bd059700649aa622423bbfa8f62cc31b3332f07d
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2023-03-18 16:38:41 +0900

    fix: axios 디폴트 credential 설정 수정

--- src/api/userStudySubject/userStudySubjectApiClient.ts
+++ src/api/userStudySubject/userStudySubjectApiClient.ts
@@ -3,5 +3,6 @@ import uriSelector from "../../utils/uriSelector";
 const userStudySubjectApiClient = axios.create();
 
 userStudySubjectApiClient.defaults.baseURL = uriSelector();
+userStudySubjectApiClient.defaults.withCredentials = true;
 
 export default userStudySubjectApiClient;
