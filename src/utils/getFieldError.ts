e37b7185079e8f135bdcc13dac823ea499eb2644
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2023-03-22 15:48:11 +0900

    fix: getFieldError 에러 조건 초기화

--- src/utils/getFieldError.ts
+++ src/utils/getFieldError.ts
@@ -2,15 +2,11 @@ function getFieldError(value: string | undefined) {
   if (!value) return "field is required";
 
   const valueIsLowerCase = value === value.toLowerCase();
-  const valueIsLongEnough = value.length >= 3;
+
   const valueIsShortEnough = value.length <= 20;
 
   if (!valueIsLowerCase) {
     return "value must be lower case";
-  } else if (!valueIsLongEnough) {
-    return "value must be at least 3 characters long";
-  } else if (!valueIsShortEnough) {
-    return "value must be no longer than 20 characters";
   }
   return null;
 }
