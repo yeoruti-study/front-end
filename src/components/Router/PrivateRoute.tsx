ed351f45e03887de19561449c01f6c502a128bfd
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2023-03-27 16:34:09 +0900

    fix: privateRoute 회원정보 없을 시 로직 수정

--- src/components/Router/PrivateRoute.tsx
+++ src/components/Router/PrivateRoute.tsx
@@ -1,21 +1,39 @@
-import React from "react";
-import {
-  Navigate,
-  Outlet,
-  redirect,
-  Route,
-  RouteProps,
-  useLocation,
-} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, RouteProps, useLocation, useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import userInfoAtom from "../../atoms/userInfo";
 import { useUserProfileGet } from "../../hooks/react_query_hooks/useUser";
 import { PrivateRouteType } from "./types/Route";
 
 const PrivateRoute = ({ children }: PrivateRouteType & RouteProps) => {
+  const navigate = useNavigate();
   const location = useLocation();
+  const setUserInfo = useSetRecoilState(userInfoAtom);
   const { status } = useUserProfileGet();
-  if (status === "error") {
-    return <Navigate to="/" replace state={{ from: location }} />;
-  } else if (status === "success") {
+  useEffect(() => {
+    if (status === "error") {
+      // alert("회원정보를 조회할 수 없습니다");
+      setUserInfo({
+        id: "",
+        username: "",
+        roles: "",
+        profileName: "",
+        profileBirth: "",
+        profileImagePath: "",
+        alarmPermission: true,
+        friendAcceptance: true,
+      });
+      navigate("/");
+    }
+  }, [status]);
+
+  // if (status === "error") {
+  //   return <Navigate to="/" replace state={{ from: location }} />;
+  // } else if (status === "success") {
+  //   return children ? children : <Outlet />;
+  // }
+
+  if (status === "success") {
     return children ? children : <Outlet />;
   }
 };
