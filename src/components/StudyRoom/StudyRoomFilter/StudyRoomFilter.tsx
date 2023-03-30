78140ee3eb38cd4b42583c29138cc709408e89bc
Yongjae Choi <yongjae.choi20@gmail.com>
2023-03-30 11:09:11 +0900

    [여럿이-51] svg 속성 warning 해결 및 스터디룸 api 업데이트 (#52)
    * fix: roomPassword attribute을 hasRoomPassword로 변경
    
    * fix: SVG warning 해결
    
    * fix: 스터디 카테고리 생성 시 카테고리 list 즉시 업데이트 구현
    
    * test: api 성공 시 list console에 출력
    
    * fix: 레이아웃 overflow 수정

--- src/components/StudyRoom/StudyRoomFilter/StudyRoomFilter.tsx
+++ src/components/StudyRoom/StudyRoomFilter/StudyRoomFilter.tsx
@@ -71,13 +71,13 @@ const StudyRoomFilter = () => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="rgba(0,0,0,0.6)"
             className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M4.5 15.75l7.5-7.5 7.5 7.5"
             />
           </svg>
